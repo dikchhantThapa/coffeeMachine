5c8ce19bb06a85dfd33faddf8fe444f87e3b8673
Dikchhant Thapa <dikchhant_thapa@yahoo.com>
2022-05-22 18:43:47 -0700

    changed features

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -7,41 +7,67 @@ public class CoffeeMachine {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Write how many ml of water the coffee machine has: ");
-        int totalWaterML = scanner.nextInt();
+        int totalMoney = 550;
+        int totalWater = 400;
+        int totalMilk = 540;
+        int totalCoffeeBeans = 120;
+        int totalCups = 9;
 
-        System.out.println("Write how many ml of milk the coffee machine has: ");
-        int totalMilkML = scanner.nextInt();
+        System.out.println("The coffee machine has: \n400 ml of water \n540 ml of milk\n" +
+                "120 g of coffee beans\n9 disposable cups\n$550 of money");
 
-        System.out.println("Write how many grams of coffee beans the coffee machine has: ");
-        int totalCoffeeBeansGM = scanner.nextInt();
+        System.out.println("Write action (buy, fill, take): ");
+        String userActionChoice = scanner.next();
 
-        System.out.println("Write how many cups of coffee you will need: ");
-        int totalCupsOfCoffeeRequired = scanner.nextInt();
-
-        int waterReqPerCupML = 200;
-        int milkReqPerCupML = 50;
-        int coffeeBeansReqPerCupGM = 15;
-
-        // actual possible per ingredient
-        int actualWaterPossible = totalWaterML / waterReqPerCupML;
-        int actualMilkPossible = totalMilkML / milkReqPerCupML;
-        int actualCoffeeBeansPossible = totalCoffeeBeansGM / coffeeBeansReqPerCupGM;
-
-        int totalCoffeePossibleActual = Math.min(actualWaterPossible, Math.min(actualMilkPossible, actualCoffeeBeansPossible));
-        // finds the minimum number of coffee that can be made
-
-        if (totalCoffeePossibleActual == totalCupsOfCoffeeRequired) {
-            System.out.println("Yes, I can make that amount of coffee");
-        }   else if (totalCoffeePossibleActual > totalCupsOfCoffeeRequired) {
-            int extraCupsCanBeMade = totalCoffeePossibleActual - totalCupsOfCoffeeRequired;
-            System.out.println("Yes, I can make that amount of coffee (and even " + extraCupsCanBeMade +
-                    " more than that)");
-        }   else {
-            System.out.println("no, I can make only " + totalCoffeePossibleActual + " cup(s) of coffee");
+        switch (userActionChoice) {
+            case "buy":
+                System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ");
+                int userMenuChoice = scanner.nextInt();
+                if (userMenuChoice == 1) {
+                    totalWater -= 250;
+                    totalCoffeeBeans -= 16;
+                    totalMoney += 4;
+                    totalCups -= 1;
+                } else if (userMenuChoice == 2) {
+                    totalWater -= 350;
+                    totalMilk -= 75;
+                    totalCoffeeBeans -= 20;
+                    totalMoney += 7;
+                    totalCups -= 1;
+                } else if (userMenuChoice == 3) {
+                    totalWater -= 200;
+                    totalMilk -= 100;
+                    totalCoffeeBeans -= 12;
+                    totalMoney += 6;
+                    totalCups -= 1;
+                }
+                break;
+            case "fill":
+                System.out.println("Write how many ml of water you want to add:");
+                int fillWater = scanner.nextInt();
+                totalWater = totalWater + fillWater;
+                System.out.println("Write how many ml of milk you want to add:");
+                int fillMilk = scanner.nextInt();
+                totalMilk = totalMilk + fillMilk;
+                System.out.println("Write how many grams of coffee beans you want to add:");
+                int fillCoffeeBeans = scanner.nextInt();
+                totalCoffeeBeans = totalCoffeeBeans + fillCoffeeBeans;
+                System.out.println("Write how many disposable cups of coffee you want to add:");
+                int fillCoffeeCups = scanner.nextInt();
+                totalCups = totalCups + fillCoffeeCups;
+                break;
+            case "take":
+                totalMoney = 0;
+                System.out.println("I gave you $" + totalMoney);
+                break;
         }
 
-
+        System.out.println("The coffee machine has: ");
+        System.out.println(totalWater + " ml of water");
+        System.out.println(totalMilk + " ml of milk");
+        System.out.println(totalCoffeeBeans + " g of coffee beans");
+        System.out.println(totalCups + " disposable cups");
+        System.out.println("$" + totalMoney + " of money");
 
     }
 }
